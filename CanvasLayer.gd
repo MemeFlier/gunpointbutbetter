a25e25c03ca760c3727f68deed6dcca0f31b564c
aayushrh <73722373+aayushrh@users.noreply.github.com>
2023-06-01 19:50:40 -0700

    Bug Fixes

--- CanvasLayer.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-var cool_input_1 = Vector2.ZERO
-
-func _input(event):
-	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if $TouchScreenButton.is_pressed():
-			cool_input_1 = (event.position-$TouchScreenButton.position-Vector2(110,110)).normalized()
-			
--- hi.gd
+++
@@ -1,3 +0,0 @@
-extends Node2D
-func _process(delta):
-	$Gamer.cool_output_1 = $TS.cool_input_1
